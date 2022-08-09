0b4e30e9bc3d10d436b6f0412b66dcc0b0267dfd
David Taccetta <taccetta14@gmail.com>
2022-08-09 09:00:56 -0400

    Java Exercise 1

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+//Exercise 1
+const firstName = "David";
+let lastName = "Something";
+var age = 24;
 
-// Exercise 2
+//Exercise 2
+lastName = "Taccetta"
+age = 25;
 
-// Exercise 3
+//Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language + " was created in " + createdYear)
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePErcentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(product + " in size " + selectedSize + " is " + price + " with " + stock + " in stock.")
